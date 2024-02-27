1ecee3d16447e499b74fc07204c05f7510f48244
白熱 <sonne@asaki.me>
2024-02-27 16:04:57 +0800

    fix: change locale file names and variable names (#4)

--- packages/esbuild-plugin/types.d.ts
+++ packages/esbuild-plugin/types.d.ts
@@ -1,4 +1,4 @@
 declare module 'univer:locales' {
-  export const en_US: { [key: string]: string }
-  export const zh_CN: { [key: string]: string }
+  export const enUS: { [key: string]: string }
+  export const zhCN: { [key: string]: string }
 }
