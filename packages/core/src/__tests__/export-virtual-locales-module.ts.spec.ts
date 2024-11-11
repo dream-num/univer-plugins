08cd83efb2adad97ef7fd8944fef27f6ece237cc
hexf00 <hexf00@gmail.com>
2024-11-11 19:00:02 +0800

    test: update test

--- packages/core/src/__tests__/export-virtual-locales-module.ts.spec.ts
+++ packages/core/src/__tests__/export-virtual-locales-module.ts.spec.ts
@@ -8,6 +8,14 @@ describe('export-virtual-locales-module', () => {
       'node_modules/@univerjs/ui/lib/types/locale/en-US.d.ts': '/* en-US locale */',
       'node_modules/@univerjs/ui/lib/types/locale/zh-CN.d.ts': '/* zh-CN locale */',
       'node_modules/@univerjs/ui/lib/types/locale/ru-RU.d.ts': '/* ru-RU locale */',
+      'node_modules/@univerjs/design/lib/types/locale/vi-VN.d.ts': '/* vi-VN locale */',
+
+      'node_modules/@univerjs/ui/lib/es/locale/en-US.js': '/* en-US locale */',
+      'node_modules/@univerjs/ui/lib/es/locale/zh-CN.js': '/* zh-CN locale */',
+      'node_modules/@univerjs/ui/lib/es/locale/ru-RU.js': '/* ru-RU locale */',
+      'node_modules/@univerjs/ui/lib/locale/ru-RU.json': '/* ru-RU locale */',
+      'node_modules/@univerjs/design/lib/locale/vi-VN.json': '/* vi-VN locale */',
+
       'node_modules/@univerjs/design/lib/index.css': '/* univerjs design css */',
     })
   })
@@ -20,9 +28,10 @@ describe('export-virtual-locales-module', () => {
     const outputCode = exportVirtualLocalesModule()
 
     expect(outputCode).toMatch(/import { Tools as _Tools } from '@univerjs\/core';/)
-    expect(outputCode).toMatch(/import [a-zA-Z]{8}enUS from '@univerjs\/ui\/lib\/locale\/en-US';/)
-    expect(outputCode).toMatch(/import [a-zA-Z]{8}ruRU from '@univerjs\/ui\/lib\/locale\/ru-RU';/)
-    expect(outputCode).toMatch(/import [a-zA-Z]{8}zhCN from '@univerjs\/ui\/lib\/locale\/zh-CN';/)
+    expect(outputCode).toMatch(/import [a-zA-Z]{8}enUS from '@univerjs\/ui\/lib\/es\/locale\/en-US.js';/)
+    expect(outputCode).toMatch(/import [a-zA-Z]{8}ruRU from '@univerjs\/ui\/lib\/es\/locale\/ru-RU.js';/)
+    expect(outputCode).toMatch(/import [a-zA-Z]{8}zhCN from '@univerjs\/ui\/lib\/es\/locale\/zh-CN.js';/)
+    expect(outputCode).toMatch(/import [a-zA-Z]{8}viVN from '@univerjs\/design\/lib\/locale\/vi-VN.json';/)
 
     expect(outputCode).toMatch(/export const enUS = _Tools.deepMerge\([a-zA-Z]{8}enUS,\n\);/)
     expect(outputCode).toMatch(/export const ruRU = _Tools.deepMerge\([a-zA-Z]{8}ruRU,\n\);/)
